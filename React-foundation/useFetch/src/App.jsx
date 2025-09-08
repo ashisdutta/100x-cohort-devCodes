f27691f58aacfc8eb8940d06783320e613d3c058
ashisdutta <ashisdutta795@gmail.com>
2025-09-08 19:22:23 +0530

    adding useFetch custom hook

---
+++ React-foundation/useFetch/src/App.jsx
@@ -0,0 +1,24 @@
+import {useFetch} from "./hooks/useFetch"
+import { useState} from "react"
+
+function App(){
+  const [currentPost, setCurrentPost] = useState(1);
+  const {finalData, loading} = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+  
+  if(loading){
+    return <div>
+      Loading....
+    </div>
+  }
+  
+  return (
+    <div>
+    <button onClick={() => setCurrentPost(1)}>1</button>
+    <button onClick={() => setCurrentPost(2)}>2</button>
+    <button onClick={() => setCurrentPost(3)}>3</button>
+    {JSON.stringify(finalData)}
+  </div>
+  )
+}
+
+export default App;
